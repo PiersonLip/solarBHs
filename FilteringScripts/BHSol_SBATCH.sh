de583bfae9c6cc41f30219b1af5371a85ffd5899
Pierson Lipschultz <piersonlipschultz@gmail.com>
2026-04-01 14:11:42 -0500

    g3 data and newer vs graphs

--- FilteringScripts/BHSol_SBATCH.sh
+++ FilteringScripts/BHSol_SBATCH.sh
@@ -9,4 +9,4 @@
 #SBATCH --mail-user=piersonlipschultz@gmail.com
 export PATH_TO_POSYDON=/home/bku2126/b1095/bku2126/UCXBInvestigation/POSYDON
 export PATH_TO_POSYDON_DATA=/projects/b1095/bku2126/POSYDON_tutorial/data
-python BH-Sol_Filter.py /home/bku2126/b1095/bku2126/stellarBHs/grids/g2/1e+00_Zsun_population.h5 -o /home/bku2126/b1095/bku2126/stellarBHs/Data/SunLikeStar_Filt/ -overwrite False --maxPeriod 300
+python BH-Sol_Filter.py /home/bku2126/b1095/bku2126/stellarBHs/grids/g3/1e+00_Zsun_population.h5 -o /home/bku2126/b1095/bku2126/stellarBHs/Data/RandInitMassDistro/ -overwrite False --maxPeriod 300
